528b329d4c3569b499b502294f56bcdd00a7b64e
SophyaL <sophyal@mail.ru>
2022-11-15 21:22:27 +0700

    change model of database

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,35 +1,44 @@
 from django.db import models
 
 
-class URL(models.Model):
-    product_ID = models.IntegerField()
-    product_URL = models.CharField(max_length=300)
-    product_shop = models.CharField(max_length=10)
+class Categories(models.Model):
+    category_ID = models.AutoField(primary_key=True)
+    category_name = models.CharField(max_length=100)
 
 
 class Info(models.Model):
-    product_ID = models.ForeignKey(URL, on_delete=models.CASCADE)
-    product_type = models.CharField(max_length=30)
-    product_name = models.CharField(max_length=200)
-    product_manufacturer = models.CharField(max_length=30)
+    product_ID = models.AutoField(primary_key=True)
+    product_category_ID = models.ForeignKey(
+        Categories,
+        on_delete=models.CASCADE
+    )
+    product_name = models.TextField()
+    product_manufacturer = models.CharField(max_length=100)
+
+
+class URL(models.Model):
+    URL_ID = models.AutoField(primary_key=True)
+    product_ID = models.ForeignKey(
+        Info,
+        on_delete=models.CASCADE
+    )
+    product_URL = models.TextField()
+    product_shop = models.CharField(max_length=100)
+
+
+class Pictures(models.Model):
+    picture_ID = models.AutoField(primary_key=True)
+    picture_URL = models.TextField()
+    product_ID = models.ForeignKey(
+        Info,
+        on_delete=models.CASCADE
+    )
 
 
 class Cost(models.Model):
-    product_ID = models.ForeignKey(URL, on_delete=models.CASCADE)
+    product_ID = models.OneToOneField(
+        Info,
+        on_delete=models.CASCADE
+    )
     product_cost = models.IntegerField()
     last_update = models.TimeField()
-
-
-class Picture(models.Model):
-    product_ID = models.ForeignKey(URL, on_delete=models.CASCADE)
-    picture_URl = models.CharField(max_length=300)
-
-# class Question(models.Model):
-#     question_text = models.CharField(max_length=200)
-#     pub_date = models.DateTimeField('date published')
-#
-#
-# class Choice(models.Model):
-#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-#     choice_text = models.CharField(max_length=200)
-#     votes = models.IntegerField(default=0)
