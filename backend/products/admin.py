db4f6dc6cd8452b33b8b8e988f679095d93e2207
SophyaL <sophyal@mail.ru>
2022-11-15 21:30:41 +0700

    add load products to database from all.json file

--- backend/products/admin.py
+++ backend/products/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from .models import URL, Info, Cost, Picture
+from .models import Categories, Info, URL, Pictures, Cost
 
-admin.site.register(URL)
+
+admin.site.register(Categories)
 admin.site.register(Info)
+admin.site.register(URL)
+admin.site.register(Pictures)
 admin.site.register(Cost)
-admin.site.register(Picture)
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from datetime import datetime
 
-# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+from products.models import Categories, Info, URL, Pictures, Cost
+import json
+
+
+def index(request):
+    f = open("scraper/all.json", encoding='utf-8')
+    data = json.load(f)
+    names = ""
+
+    for i in data:
+        if not (Categories.objects.filter(category_name=i["category"])):
+            c = Categories(category_name=i["category"])
+            c.save()
+
+        c = Categories.objects.filter(category_name=i["category"]).first()
+
+        if URL.objects.filter(product_URL=i["url"]):
+            product = URL.objects.filter(product_URL=i["url"]).first().product_ID
+            product.delete()
+
+        product = Info(product_category_ID=c,
+                       product_name=i["name"],
+                       product_manufacturer=i["manufacturer"]
+                       )
+        product.save()
+
+        for pict in i["pictures"]:
+            picture = Pictures(picture_URL=pict, product_ID=product)
+            picture.save()
+
+        urlI = URL(product_ID=product,
+                   product_URL=i["url"],
+                   product_shop=i["shop"]
+                   )
+        urlI.save()
+
+        cost = Cost(product_ID=product,
+                    product_cost=i["cost"],
+                    last_update=datetime.now()
+                    )
+        cost.save()
+
+        names += i["name"] + "<br>"
+    return HttpResponse("БАЗИРУЕМСЯ.............. <br>" + names)
