5fc8d7f517a5f50eb5a600d71bce660c0c489a53
m1Myp <55120919+m1Myp@users.noreply.github.com>
2022-12-13 17:07:08 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import json
 import numpy
-# Чтение из JSON файлов
-# С последующим сравнением штук.
-# Добро пожаловать на сервер шизофрения
 from pprint import pprint
 import re
 from string import ascii_letters as en
@@ -27,41 +24,36 @@ engNotNeedWord = ["usb", "провдная", "проводной"]
 for input_txt in txtBase:
     with open(input_txt, encoding='utf-8', mode="r") as json_file:
         data = json.loads(json_file.read())
-        print(data)
         for p in data:
             name = p["name"].lower()
             newTempStuff = re.sub("\[.*?\]","", name)
-            newTempStuff = newTempStuff.split()  # Темпик для создания токенов с помощью сплита, чтобы не каверкать изначальный нейм продукта
-            # Сплит производится по пробелу
-            anotherTempStuff = ["", "", "", ""]  # Хранитель актуальных токенов в системе
-            # fuzz.token_sort_ratio еще кринжовее?
+            newTempStuff = newTempStuff.split()  
+            anotherTempStuff = ["", "", "", ""]
             for tempName in newTempStuff:
                 subname = ''.join(
-                    ch for ch in tempName if ch.isalnum())  # Создание токена без мусора только символы и цифры
+                    ch for ch in tempName if ch.isalnum()) 
                 breakFlag = False
-                for color in colors.values():  # Проход по словарю цветов, если есть совпадение добавляем в буфер токена на позицию 2
+                for color in colors.values():
                     if subname in color:
                         anotherTempStuff[2] = color[0]
                         breakFlag = True
                         break
                 if breakFlag:
                     continue
-                for characteristic in characteristics.values():  # Проход по словарю характеристик, если есть совпадение добавляем в буфер токена на позицию 3
+                for characteristic in characteristics.values(): 
                     if subname == characteristic:
                         anotherTempStuff[3] = subname
                         breakFlag = True
                         break
                 if breakFlag:
                     continue
-                if any(map(str.isdigit,
-                           subname)):  # Если в токене есть хотя бы 1 символ он пишется в буфер токена на позицию 1
-                    # Нужно реализовать проверку на наличие в слове скобочек, для игнорирования рандомного кода товара внутри названия
+                if any(map(str.isdigit, subname)):
                     anotherTempStuff[1] += subname
                     breakFlag = True
                 if breakFlag:
                     continue
-                if re.search(r'[a-z0-9]', subname):  # Если в токене английские буквы, то это название товара
-                    if (subname in engNotNeedWord):  # Проверка что это не английское слово которое нам не нужно, типо USB/HDMI/...
+                if re.search(r'[a-z0-9]', subname): 
+                    if (subname in engNotNeedWord): 
                         continue
                     anotherTempStuff[0] += subname
             id = 0
@@ -91,16 +83,3 @@ for input_txt in txtBase:
                     dataBase[finalName+anotherTempStuff[1]+anotherTempStuff[2]+anotherTempStuff[3]] = [p['link']]
                     newBuferIwannaToImportToJSONLater.append(anotherTempStuff)
 pprint(dataBase)
-
-# Сюда надо добавить сохранение в буфер anotherTempStuff перед его записью для сравнения
-# Вот здесь токены должны сравниваться с токенами которые я запишу ранее в отдельный файлик
-# Токены проверяются по порядку
-# Разное имя -> новый товар ИНАЧЕ
-# Разный "номер товара" -> новый товар ИНАЧЕ
-# Разный цвет -> новый товар ИНАЧЕ
-# Цвет пуст -> Что-то делаем чтобы сравнить с токенами которые уже существуют
-# Мои 2 варианта - превращать пустой токен во все токены которые позволяет словарь цветов, ИЛИ
-# Сделать токены пустыми в тех вещах которые мы сравниваем с этим товаром
-# Проблема и того и того метода, что он работает до первого вхождения, что не есть good
-# Если цвет не пуст и совпадает идем дальше
-# Смотри пункт про цвета, только это пункт про "характеристики" а.ля беспроводная
